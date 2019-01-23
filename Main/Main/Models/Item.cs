9c4d184bb6d13334a03ec07629cfdb3750547a2c
griffinpuc <griffinpuc7@gmail.com>
2019-01-23 17:14:50 +0000

    Additional metadata added to <Item> object for inventory page

--- Main/Main/Models/Item.cs
+++ Main/Main/Models/Item.cs
@@ -10,6 +10,7 @@ namespace Main.Models
         public int Code { get; set; }
         public string Name { get; set; }
         public string Desc { get; set; }
+        public double WPrice { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
         public string Category { get; set; }
--- Main/Main/Models/TestItemRepo.cs
+++ Main/Main/Models/TestItemRepo.cs
@@ -11,9 +11,12 @@ namespace Main.Models
 
         public IQueryable<Item> Items => new List<Item>
         {
-            new Item {Code = 1, Name = "Plate", Desc = "A normal plate", Price = 15.99, Quantity = 150, Category = "Kitchen"},
-            new Item {Code = 2, Name = "Bowl", Desc = "A normal bowl", Price = 10.29, Quantity = 150, Category = "Kitchen"},
-            new Item {Code = 3, Name = "Napkins", Desc = "Pile of napkins", Price = 5.32, Quantity = 50, Category = "Kitchen"}
+            new Item {Code = 1, Name = "Plate", Desc = "A normal plate", WPrice = 9.30, Price = 15.99, Quantity = 150, Category = "Kitchen"},
+            new Item {Code = 2, Name = "Bowl", Desc = "A normal bowl",  WPrice = 3.20, Price = 10.29, Quantity = 150, Category = "Kitchen"},
+            new Item {Code = 3, Name = "Napkins", Desc = "Pile of napkins",  WPrice = 1.49, Price = 5.32, Quantity = 50, Category = "Kitchen"},
+            new Item {Code = 4, Name = "Shower Gel", Desc = "Bottle of shower gel",  WPrice = 0.76, Price = 3.99, Quantity = 43, Category = "Bath"},
+            new Item {Code = 5, Name = "Carrot", Desc = "Singular orange veggie",  WPrice = 0.09, Price = 0.39, Quantity = 150, Category = "Produce"},
+            new Item {Code = 6, Name = "White Cotton Shirt", Desc = "A normal plain shirt",  WPrice = 1.23, Price = 5.99, Quantity = 32, Category = "Clothing"}
 
         }.AsQueryable<Item>();
     }
