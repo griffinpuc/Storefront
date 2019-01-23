41f9636328f9ac1081063afd1e4e9e18dd882666
griffinpuc <griffinpuc7@gmail.com>
2019-01-23 16:09:55 +0000

    additional frontend formatting to inventory list

--- Main/Main/Models/Item.cs
+++ Main/Main/Models/Item.cs
@@ -7,11 +7,12 @@ namespace Main.Models
 {
     public class Item
     {
-        public int ID { get; set; }
+        public int Code { get; set; }
         public string Name { get; set; }
         public string Desc { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
+        public string Category { get; set; }
 
     }
 }
--- Main/Main/Models/TestItemRepo.cs
+++ Main/Main/Models/TestItemRepo.cs
@@ -7,11 +7,13 @@ namespace Main.Models
 {
     public class TestItemRepo : IItemRepo
     {
+
+
         public IQueryable<Item> Items => new List<Item>
         {
-            new Item {ID = 1, Name = "Plate", Desc = "A normal plate", Price = 15.00, Quantity = 150},
-            new Item {ID = 1, Name = "Bowl", Desc = "A normal bowl", Price = 10.00, Quantity = 150},
-            new Item {ID = 1, Name = "Napkins", Desc = "Pile of napkins", Price = 5.00, Quantity = 50}
+            new Item {Code = 1, Name = "Plate", Desc = "A normal plate", Price = 15.99, Quantity = 150, Category = "Kitchen"},
+            new Item {Code = 2, Name = "Bowl", Desc = "A normal bowl", Price = 10.29, Quantity = 150, Category = "Kitchen"},
+            new Item {Code = 3, Name = "Napkins", Desc = "Pile of napkins", Price = 5.32, Quantity = 50, Category = "Kitchen"}
 
         }.AsQueryable<Item>();
     }
